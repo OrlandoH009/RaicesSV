abbeb0190589cbdf8aef5b57311975f5ea9ca5ba
rickkktt <ricagamer125@gmail.com>
2026-06-28 23:25:45 -0600

    Adición 'temporal' de confirmación de cuando la bd conecte

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const pool = require('./data/config/connection');
 
 dotenv.config();
 
@@ -10,6 +11,15 @@ app.use(express.json());
 
 app.get('/estado', (req, res) => {
     res.json({ status: 'ok', message: 'Servidor funcionando' });
+});
+
+app.get('/test-db', async (req, res) => {
+try {
+    const [rows] = await pool.query('SELECT 1 + 1 AS solution');
+    res.json({db: true, result: rows[0].result});
+} catch (e) {
+        res.status(500).json({db: false, error: e.message});
+    }
 });
 
 app.listen(port, () => {
