61fa8a67b24468603175ddb83c4da94dec8a96e6
rickktt <ricagamer125@gmail.com>
2026-07-01 11:45:04 -0600

    Función de await  y async con promises de mysql2 aplicada

--- data/repositories/user.repository.js
+++ data/repositories/user.repository.js
@@ -1,40 +1,19 @@
 const db = require('../config/database.config');
 
-const findByEmail = (email) => {
-    return new Promise((resolve, reject) => {
-
-        db.query(
-            'SELECT * FROM users WHERE email = ?',
-            [email],
-            (err, results) => {
-
-                if (err) return reject(err);
-
-                resolve(results[0]);
-            }
-        );
-
-    });
+const findByEmail = async (email) => {
+    const [rows] = await db.query(
+        'SELECT * FROM users WHERE email = ?',
+        [email]
+    );
+    return rows[0];
 };
 
-const createUser = (name, email, password) => {
-    return new Promise((resolve, reject) => {
-
-        db.query(
-            'INSERT INTO users(name,email,password) VALUES (?,?,?)',
-            [name, email, password],
-            (err, result) => {
-
-                if (err) return reject(err);
-
-                resolve(result);
-            }
-        );
-
-    });
+const createUser = async (name, email, password) => {
+    const [result] = await db.query(
+        'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+        [name, email, password]
+    );
+    return result;
 };
 
-module.exports = {
-    findByEmail,
-    createUser
-};
+module.exports = { findByEmail, createUser };
