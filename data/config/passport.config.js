1ee633d5e38e7a616c6e673e1bd4ac398233b2de
rickkktt <ricagamer125@gmail.com>
2026-07-05 23:40:13 -0600

    Adición de try catch para detectar errores en done en la autenticación con google

--- data/config/passport.config.js
+++ data/config/passport.config.js
@@ -6,5 +6,15 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
+    const name = profile.displayName;
+    const email = profile.emails[0].value;
+    const googleId = profile.id;
+
+    try {
+        const user = await authService.loginOrRegisterWithGoogle(name, email, googleId);
+        done(null, user);
+    } catch (e) {
+        done(e);
+    }
 }));
