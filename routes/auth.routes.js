996c11bec3523fabbea8c74cc0d4b17125e74077
rickktt <ricagamer125@gmail.com>
2026-07-06 09:09:29 -0600

    Hacer que el usuario al iniciar con google no sea directo, sino que siempre entre al auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,7 +7,7 @@ const googleCallback = require('../business/auth.google');
 
 router.post('/login', login);
 router.post('/register', register);
-router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email'], session: false}));
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email'], session: false, prompt: 'select_account'}));
 router.get('/auth/google/callback', passport.authenticate('google', { session: false, failureRedirect: '/login.html' }), googleCallback);
 
 module.exports = router;
