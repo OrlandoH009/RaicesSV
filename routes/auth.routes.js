09a0c25ffdf937cfb2be11a7980e9da2040da51b
rickkktt <ricagamer125@gmail.com>
2026-07-06 00:05:43 -0600

    Adición de rutas en caso de que el login falle o si llegará a funcionar, para tomar los datos de sesión

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const login = require('../business/auth.login');
 const register = require('../business/auth.register');
+const googleCallback = require('../business/auth.google');
 
 router.post('/login', login);
 router.post('/register', register);
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email'], session: false}));
+router.get('/auth/google/callback', passport.authenticate('google', { session: false, failureRedirect: '/login.html' }), googleCallback);
 
 module.exports = router;
