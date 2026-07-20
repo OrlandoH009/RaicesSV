185da47de8555f1bc2d3fdbc6a7f8968befb9494
rickktt <ricagamer125@gmail.com>
2026-07-20 09:35:51 -0600

    Verificación de la url en los datos de sesión en auth google

--- business/auth.google.js
+++ business/auth.google.js
@@ -1,31 +1,27 @@
 const googleCallback = (req, res) => {
     const user = req.user;
 
+    const redirectAfterLogin = req.session.postLoginRedirect || '/';
     const sessionData = {
         id: user.id_user,
         name: user.name,
         email: user.email,
         avatarUrl: user.avatar_url || null
     };
-
     req.session.regenerate((regenErr) => {
         if (regenErr) {
             console.error(regenErr);
             return res.status(500).send('No se pudo iniciar sesión. Intentelo de nuevo más tarde.');
         }
-
         req.session.user = sessionData;
-
         req.session.save((saveErr) => {
             if (saveErr) {
                 console.error(saveErr);
                 return res.status(500).send('No se pudo iniciar sesión. Intentelo de nuevo más tarde.');
             }
 
-            res.redirect('/');
+            res.redirect(redirectAfterLogin);
         });
     });
-
 }
-
 module.exports = googleCallback;
