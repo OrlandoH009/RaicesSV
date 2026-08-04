2bcfbff55dfb47d138a7c81854d2b392ce93216d
rickkktt <ricagamer125@gmail.com>
2026-08-04 15:32:23 -0600

    Creación de el archivo de rutas para el funcionamiento del crud según rol

---
+++ routes/publications.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const requireApiAuth = require('../middleware/auth.apiGuard');
+const upload = require('../middleware/upload.publications.middleware');
+
+const listPublications = require('../business/publication.list');
+const getPublication = require('../business/publication.get');
+const createPublication = require('../business/publication.create');
+const updatePublication = require('../business/publication.update');
+const deletePublication = require('../business/publication.delete');
+
+router.get('/api/publications', listPublications);
+router.get('/api/publications/:id', getPublication);
+
+router.post('/api/publications', requireApiAuth, upload.single('image'), createPublication);
+router.put('/api/publications/:id', requireApiAuth, upload.single('image'), updatePublication);
+router.delete('/api/publications/:id', requireApiAuth, deletePublication);
+
+module.exports = router;
