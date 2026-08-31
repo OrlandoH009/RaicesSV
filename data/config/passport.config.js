afe81e87e1dd3b8f3bd4da2d3cb06bb4a038dea9
rickkktt <ricagamer125@gmail.com>
2026-08-30 23:28:32 -0600

    Arreglo de bug en passport para añadir al try y al catch el correo para que si google no envie nada tire una excepción y no se congele

--- data/config/passport.config.js
+++ data/config/passport.config.js
@@ -7,12 +7,18 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
-    const name = profile.displayName;
-    const email = profile.emails[0].value;
-    const googleId = profile.id;
-    const googlePhotoUrl = profile.photos && profile.photos[0] ? profile.photos[0].value : null;
-
     try {
+        const name = profile.displayName;
+        const email = profile.emails && profile.emails[0] ? profile.emails[0].value : null;
+        const googleId = profile.id;
+        const googlePhotoUrl = profile.photos && profile.photos[0] ? profile.photos[0].value : null;
+
+        if (!email) {
+            const e = new Error('No se pudo obtener tu correo de Google. Verifica los permisos otorgados e inténtalo de nuevo.');
+            e.expose = true;
+            throw e;
+        }
+
         const user = await authService.loginOrRegisterWithGoogle(name, email, googleId, googlePhotoUrl);
         done(null, user);
     } catch (e) {
